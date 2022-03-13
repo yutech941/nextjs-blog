bcdc4a19476939639d44571811c72cc97641ffcb
小林優一 <y.k94cofitec179@gmail.com>
2022-03-14 08:03:10 +0900

    getStaticPropsを使用してみた。

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>小林です。東京でWebのエンジニアをやっています。</p>
+        <p>小林です。東京でWebのエンジニアをやっています。フリーランスです。</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
